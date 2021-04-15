b0579ae0c1aeeacb6ee75993fc01ba8856fdb0dc
Imogen Connor-Helleur <imooconnor@outlook.com>
2021-04-15 17:23:53 +0100

    Moved R files into R folder.

--- installation_command.R
+++
@@ -1,5 +0,0 @@
-#Run this to install the hospitalflow package
-devtools::install_github("HorridTom/hospitalflow", ref = "update-vignette",
-                         auth_token =  , 
-                         build = F
-                         )
